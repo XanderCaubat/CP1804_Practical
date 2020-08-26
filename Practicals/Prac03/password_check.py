5767b50c795ecb1338add725744550b97c8b5c18
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2020-08-26 14:41:52 +1000

    Refactored password check program.
    Refactor password check program to use functions

--- Practicals/Prac03/password_check.py
+++ Practicals/Prac03/password_check.py
@@ -1,7 +1,20 @@
-password = input("Password: ")
-while len(password) < 2 or len(password) > 5:
-    print("Invalid length.")
+def main():
+    password = get_password()
+    while len(password) < 2 or len(password) > 5:
+        print("Invalid length.")
+        password = get_password()
+    print_asterisk(password)
+
+
+def print_asterisk(password):
+    if len(password) > 2 or len(password) < 5:
+        for i in range(0, len(password)):
+            print("*", end='')
+
+
+def get_password():
     password = input("Password: ")
-if len(password) > 2 or len(password) < 5:
-    for i in range(0, len(password)):
-        print("*", end='')
+    return password
+
+
+main()
