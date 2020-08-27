eb63ee433f0594d28416ecfe208455e77b7359ed
Xander Caubat <xanderdino.caubat@my.jcu.edu.au>
2020-08-27 15:04:01 +1000

    prac04

---
+++ Practicals/Prac04/lists_warmup.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Lists "warm-up"
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+#   3
+#   2
+#   1
+#   3:2
+#   1:5
+#   True
+#   False
+#   "1"
+#   39
+
+#   1.  numbers[0] = 10
+#   2.  numbers[3:4] = 1
+#   3.  numbers[2:6]
+#   4. 9 in numbers
